ba3d7d38bd102048d879129efa952c5a76b49360
Roman100500 <chizhkinra001@mail.ru>
2023-03-17 22:27:43 +0300

    Lesson 1

---
+++ Lesson_1/script.js
@@ -0,0 +1,75 @@
+// Task 1
+/* 
+console.log("Текст из script.js");
+*/
+// Task 2
+/* 
+const num = 123;
+console.log(num);
+let a;
+a = 5;
+a = 7; //переприсвоили a
+const b = 1 + 2 + 3;
+console.log(a);
+console.log(b);
+*/
+
+/* 
+   1.Первый вид комментария 
+*/
+
+// 2.Второй вид комментария
+
+// Task 3
+/* 
+const c = 10;
+const d = 2;
+const sum = c + d;
+const sub = c - d;
+const mult = c * d;
+const div = c / d;
+const res = sum + sub + mult + div;
+console.log("Результат всех действий: " + sum, sub, mult, div);
+console.log(
+  `Cумма: ${sum}\nРазность: ${sub}\nУмножение: ${mult}\nДеление: ${div}\n`
+); // \n - начинает с новой строки \t - табуляция
+console.log(`Сумма всех действий: ${res}`);
+*/
+
+// Task 4
+/* 
+let a = 5 + 5 * 3 + 3;
+console.log(a);
+let b = 8 / 2 + 2;
+console.log(b);
+let c = (2 + 3) * (2 + 3);
+console.log(c);
+let d = (2 + 3) * 2 + 3;
+console.log(d);
+let e = (2 * 8) / 4;
+console.log(e);
+let f = 2 * (8 / 4);
+console.log(f);
+*/
+
+// Task 5
+/* 
+const [a, b] = [1.5, 0.75]; // Деструктуризация
+console.log(a + b);
+const c = 28;
+console.log(-c);
+console.log(-1 * c); //тоже самое что и строчка выше
+*/
+
+// Task 6
+/* 
+const firstName = "Ivan";
+const lastName = "Petrov";
+console.log(firstName + " " + lastName);
+const java = "java";
+const script = "script";
+console.log(java + script);
+const hello = "Hello";
+const world = "World";
+console.log(`${hello}, ${world}!`);
+*/
